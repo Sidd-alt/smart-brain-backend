25a72d7bb86d76b4c07697e18b68625e9eaac03d
SIDDHANT VINCHURKAR <you@example.com>
2022-09-05 17:45:57 +0530

    Added database connection

--- server.js
+++ server.js
@@ -10,11 +10,9 @@ const clarifai = require('./controllers/clarifai');
 
 const db = knex({
    client: 'pg',
-   connection: {
-      host: '127.0.0.1',
-      user: 'postgres',
-      password: "siddhant",
-      database: 'smart-brain-db'
+   connectionString: process.env.DATABASE_URL,
+   ssl: {
+     rejectUnauthorized: false
    }
 });
 
